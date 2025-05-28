7006e4abb238eb5a3dbd6a0f204eb47048d2ad56
bianckaa <bianckaraxon@gmail.com>
2025-05-28 07:03:40 -0600

    pruebas unitarias recomendaciones

--- src/app.py
+++ src/app.py
@@ -1,12 +1,17 @@
+from recommendation_manager import RecommendationManager
+from db_connection import URI, USER, PASSWORD
+from flask import jsonify
 from flask import Flask, render_template, request
 import requests
 import os
+import atexit
 
 # Ruta absoluta hacia src/templates
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  
 template_dir = os.path.join(base_dir, 'templates')
 
 app = Flask(__name__)
+recommendation_manager = RecommendationManager(URI, USER, PASSWORD)
 
 @app.route("/")
 def index():
@@ -28,5 +33,28 @@ def get_weather():
             return "❌ No se pudo obtener el clima para las coordenadas brindadas."
     return "⚠️ Coordenadas no recibidas."
 
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    data = request.json
+    username = data.get('user')
+    estilo = data.get('estilo')
+    clima = data.get('clima')
+    ocasion = data.get('ocasion')
+
+    # Validar datos mínimos
+    if not all([username, estilo, clima, ocasion]):
+        return jsonify({"error": "Faltan datos para generar recomendaciones"}), 400
+
+    prendas = recommendation_manager.get_recommendations(username, estilo, clima, ocasion)
+    if not prendas:
+        return jsonify({"message": "No se encontraron recomendaciones para esos parámetros"}), 404
+
+    return jsonify(prendas)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+@atexit.register
+def shutdown():
+    recommendation_manager.close()
---
+++ src/recommendation_manager.py
@@ -0,0 +1,35 @@
+import sys
+from dotenv import load_dotenv
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from db_connection import Neo4jApp
+
+# Cargar variables del archivo .env
+load_dotenv()
+
+# Obtener valores del archivo .env
+URI = os.getenv("NEO4J_URI")
+USER = os.getenv("NEO4J_USER")
+PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+class RecommendationManager:
+    def __init__(self, app):
+        self.app = app
+
+    def get_recommendations(self, estilo, clima):
+        query = """
+        MATCH (o:Outfit)-[:PERTENECE_A]->(s:Style {nombre: $estilo}),
+              (o)-[:ADEQUADO_PARA]->(c:Climate {Name: $clima})
+        RETURN o.Name AS Name, o.ID_Image AS ID_Image, o.Upper AS Upper, 
+               o.Lower AS Lower, o.Footwear AS Footwear, o.Accesory AS Accesory
+        LIMIT 5
+        """
+        with self.app.driver.session() as session:
+            result = session.run(query, {
+                "estilo": estilo,
+                "clima": clima
+            })
+            return [dict(row) for row in result]
+
+    def close(self):
+        self.app.close()
